f5e04d9199227b8db7cdc933a707b554ebaaa853
ulrichga <ulrichg@student.ethz.ch>
2020-03-09 17:35:46 +0100

    Made sure the rawreads have their original names before and after demultiplexing.

--- analysis/2-1_demultiplex/00_initialize.sh
+++ analysis/2-1_demultiplex/00_initialize.sh
@@ -5,3 +5,29 @@ if [ ! -e ./results/demultiplexed/pool_${pl} ]  ; then mkdir ./results/demultipl
 done
 if [ ! -e ./results/demultiplexed/pool_6/duplicates ]  ; then mkdir ./results/demultiplexed/pool_6/duplicates ; fi
 if [ ! -e ./results/demultiplexed/pool_12/duplicates ]  ; then mkdir ./results/demultiplexed/pool_12/duplicates ; fi
+
+# Change the names of the raw read to make their handling easier.
+mv ./data/rawreads/*ID_1-36*R1* ./data/rawreads/pool_1_R1.fastq.gz
+mv ./data/rawreads/*ID_1-36*R2* ./data/rawreads/pool_1_R2.fastq.gz
+mv ./data/rawreads/*ID_37-72*R1* ./data/rawreads/pool_2_R1.fastq.gz
+mv ./data/rawreads/*ID_37-72*R2* ./data/rawreads/pool_2_R2.fastq.gz
+mv ./data/rawreads/*ID_73-108*R1* ./data/rawreads/pool_3_R1.fastq.gz
+mv ./data/rawreads/*ID_73-108*R2* ./data/rawreads/pool_3_R2.fastq.gz
+mv ./data/rawreads/*ID_109-144*R1* ./data/rawreads/pool_4_R1.fastq.gz
+mv ./data/rawreads/*ID_109-144*R2* ./data/rawreads/pool_4_R2.fastq.gz
+mv ./data/rawreads/*ID_145-180*R1* ./data/rawreads/pool_5_R1.fastq.gz
+mv ./data/rawreads/*ID_145-180*R2* ./data/rawreads/pool_5_R2.fastq.gz
+mv ./data/rawreads/*ID_181-216*R1* ./data/rawreads/pool_6_R1.fastq.gz
+mv ./data/rawreads/*ID_181-216*R2* ./data/rawreads/pool_6_R2.fastq.gz
+mv ./data/rawreads/*ID_217-252*R1* ./data/rawreads/pool_13_R1.fastq.gz
+mv ./data/rawreads/*ID_217-252*R2* ./data/rawreads/pool_13_R2.fastq.gz
+mv ./data/rawreads/*ID_253-288*R1* ./data/rawreads/pool_14_R1.fastq.gz
+mv ./data/rawreads/*ID_253-288*R2* ./data/rawreads/pool_14_R2.fastq.gz
+mv ./data/rawreads/*ID_289-324*R1* ./data/rawreads/pool_9_R1.fastq.gz
+mv ./data/rawreads/*ID_289-324*R2* ./data/rawreads/pool_9_R2.fastq.gz
+mv ./data/rawreads/*ID_325-360*R1* ./data/rawreads/pool_10_R1.fastq.gz
+mv ./data/rawreads/*ID_325-360*R2* ./data/rawreads/pool_10_R2.fastq.gz
+mv ./data/rawreads/*ID_361-384*R1* ./data/rawreads/pool_11_R1.fastq.gz
+mv ./data/rawreads/*ID_361-384*R2* ./data/rawreads/pool_11_R2.fastq.gz
+mv ./data/rawreads/*ID_193-216*R1* ./data/rawreads/pool_12_R1.fastq.gz
+mv ./data/rawreads/*ID_193-216*R2* ./data/rawreads/pool_12_R2.fastq.gz
--- analysis/2-1_demultiplex/02_cleanup.sh
+++ analysis/2-1_demultiplex/02_cleanup.sh
@@ -52,3 +52,29 @@ rmdir results/demultiplexed/pool_11
 rmdir results/demultiplexed/pool_12
 rmdir results/demultiplexed/pool_13
 rmdir results/demultiplexed/pool_14
+
+# Rename the raw reads such that they are as before the demultiplexing.
+mv ./data/rawreads/pool_1_R1.fastq.gz ./data/rawreads/20191118.A-GU_ddRAD_ID_1-36_S1_R1.fastq.gz
+mv ./data/rawreads/pool_1_R2.fastq.gz ./data/rawreads/20191118.A-GU_ddRAD_ID_1-36_S1_R2.fastq.gz
+mv ./data/rawreads/pool_2_R1.fastq.gz ./data/rawreads/20191118.A-GU_ddRAD_ID_37-72_S2_R1.fastq.gz
+mv ./data/rawreads/pool_2_R2.fastq.gz ./data/rawreads/20191118.A-GU_ddRAD_ID_37-72_S2_R2.fastq.gz
+mv ./data/rawreads/pool_3_R1.fastq.gz ./data/rawreads/20191118.A-GU_ddRAD_ID_73-108_S3_R1.fastq.gz
+mv ./data/rawreads/pool_3_R2.fastq.gz ./data/rawreads/20191118.A-GU_ddRAD_ID_73-108_S3_R2.fastq.gz
+mv ./data/rawreads/pool_4_R1.fastq.gz ./data/rawreads/20191118.A-GU_ddRAD_ID_109-144_S4_R1.fastq.gz
+mv ./data/rawreads/pool_4_R2.fastq.gz ./data/rawreads/20191118.A-GU_ddRAD_ID_109-144_S4_R2.fastq.gz
+mv ./data/rawreads/pool_5_R1.fastq.gz ./data/rawreads/20191118.A-GU_ddRAD_ID_145-180_S5_R1.fastq.gz
+mv ./data/rawreads/pool_5_R2.fastq.gz ./data/rawreads/20191118.A-GU_ddRAD_ID_145-180_S5_R2.fastq.gz
+mv ./data/rawreads/pool_6_R1.fastq.gz ./data/rawreads/20191118.A-GU_ddRAD_ID_181-216_S6_R1.fastq.gz
+mv ./data/rawreads/pool_6_R2.fastq.gz ./data/rawreads/20191118.A-GU_ddRAD_ID_181-216_S6_R2.fastq.gz
+mv ./data/rawreads/pool_13_R1.fastq.gz ./data/rawreads/20191118.A-GU_ddRAD_ID_217-252_S7_R1.fastq.gz
+mv ./data/rawreads/pool_13_R2.fastq.gz ./data/rawreads/20191118.A-GU_ddRAD_ID_217-252_S7_R2.fastq.gz
+mv ./data/rawreads/pool_14_R1.fastq.gz ./data/rawreads/20191118.A-GU_ddRAD_ID_253-288_S8_R1.fastq.gz
+mv ./data/rawreads/pool_14_R2.fastq.gz ./data/rawreads/20191118.A-GU_ddRAD_ID_253-288_S8_R2.fastq.gz
+mv ./data/rawreads/pool_9_R1.fastq.gz ./data/rawreads/20191118.A-GU_ddRAD_ID_289-324_S9_R1.fastq.gz
+mv ./data/rawreads/pool_9_R2.fastq.gz ./data/rawreads/20191118.A-GU_ddRAD_ID_289-324_S9_R2.fastq.gz
+mv ./data/rawreads/pool_10_R1.fastq.gz ./data/rawreads/20191118.A-GU_ddRAD_ID_325-360_S10_R1.fastq.gz
+mv ./data/rawreads/pool_10_R2.fastq.gz ./data/rawreads/20191118.A-GU_ddRAD_ID_325-360_S10_R2.fastq.gz
+mv ./data/rawreads/pool_11_R1.fastq.gz ./data/rawreads/20191118.A-GU_ddRAD_ID_361-384_S11_R1.fastq.gz
+mv ./data/rawreads/pool_11_R2.fastq.gz ./data/rawreads/20191118.A-GU_ddRAD_ID_361-384_S11_R2.fastq.gz
+mv ./data/rawreads/pool_12_R1.fastq.gz ./data/rawreads/20191118.A-GU_ddRAD_ID_193-216_S12_R1.fastq.gz
+mv ./data/rawreads/pool_12_R2.fastq.gz ./data/rawreads/20191118.A-GU_ddRAD_ID_193-216_S12_R2.fastq.gz
